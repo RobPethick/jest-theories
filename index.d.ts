2d99c55788efaf4ede0f1f42b6c74672be1c92da
Rob Pethick <rob.pethick@infomentor.co.uk>
2019-04-09 09:17:39 +0100

    Improve typing

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,8 @@
-declare namespace jestTheories {
-    /**
-     * Runs the jest test function with each of the given theories
-     * @param description the description of the test
-     * @param theories theories passed to the test
-     * @param testFunction the test which takes a single theory parameter for each time it is executed
-     */
-    export function theoretically(description: string, theories: any[], testFunction: (theory: any) => void): void
-}
+/**
+ * Runs the jest test function with each of the given theories
+ * @param description the description of the test
+ * @param theories theories passed to the test
+ * @param testFunction the test which takes a single theory parameter for each time it is executed
+ */
+declare function theoretically(description: string, theories: any[], testFunction: (theory: any) => void): void
+export default theoretically

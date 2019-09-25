3e30263652a846688e8a8e403efd65a38630495c
Wolfgang Mayr <wolfgangmayr798@gmail.com>
2019-09-25 16:08:22 +0200

    Added counter to format parameters

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 import format from 'string-format';
 
 const theoretically = (testName, theories, testFunc) => {
-    if(!test){
+    if (!test) {
         throw new Error('Jest test global must be accessible to use jest-theories');
     }
-    if(!Array.isArray(theories)){
-        throw new Error('Theories must be an array (was '+ typeof theories + ')');
+    if (!Array.isArray(theories)) {
+        throw new Error('Theories must be an array (was ' + typeof theories + ')');
     }
-    
-    theories.forEach(theory => {
-        test(format(testName, theory), testFunc.bind(this, theory));
+
+    theories.forEach((theory, idx) => {
+        test(
+            format(testName, Object.assign({}, theory, { "$idx": idx, "$no": idx + 1 })),
+            testFunc.bind(this, theory)
+        );
     });
 };
 

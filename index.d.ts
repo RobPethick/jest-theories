4d8c60a481ddbf29ee82745843a7037403404299
Rob Pethick <rob.pethick@infomentor.co.uk>
2019-10-03 15:20:20 +0100

    #6 add in function based test name creation

--- index.d.ts
+++ index.d.ts
@@ -4,5 +4,5 @@
  * @param theories theories passed to the test
  * @param testFunction the test which takes a single theory parameter for each time it is executed
  */
-declare function theoretically<T>(description: string, theories: T[], testFunction: (theory: T) => void): void;
+declare function theoretically<T>(description: string | ((theory: T, index: number) => string), theories: T[], testFunction: (theory: T) => void): void;
 export default theoretically;
